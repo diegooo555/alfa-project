3c30ab22082097de38fec78a89a69544344fa46a
diego555uptc <diego.sanchez20@uptc.edu.co>
2024-07-05 14:12:25 -0500

    New server backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instanceAxios = axios.create({
-    baseURL: "http://localhost:4000/api",
+    baseURL: "https://alfa-project.onrender.com/api",
     withCredentials: true
 });
 
