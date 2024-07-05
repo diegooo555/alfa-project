a5f2c3e7b7c5a084a505968b44ab07d00a962823
diego555uptc <diego.sanchez20@uptc.edu.co>
2024-07-05 16:18:31 -0500

    retoques manin

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 export const PORT = process.env.PORT || 4000;
 export const MONGODB_URI =
   process.env.MONGODB_URI || "mongodb+srv://alfa555:alfa555@cluster0.abcrvnl.mongodb.net";
-export const TOKEN_SECRET = process.env.TOKEN_SECRET || "some secret key";
+export const TOKEN_SECRET = process.env.TOKEN_SECRET || "yoursecret";
 
 export const FRONTEND_URL = process.env.FRONTEND_URL || "http://localhost:5173";
