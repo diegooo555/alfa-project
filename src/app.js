51a270a7db4dfbe09ec26f89543ad71efcde39bd
diego555uptc <diego.sanchez20@uptc.edu.co>
2024-07-06 22:53:35 -0500

    change structure of app.js

--- src/app.js
+++ src/app.js
@@ -23,14 +23,5 @@ app.use('/api' ,authRoutes);
 
 app.use('/api', tasks)
 
-if (process.env.NODE_ENV === "production") {
-    const path = await import("path");
-    app.use(express.static("client/dist"));
-  
-    app.get("*", (req, res) => {
-      console.log(path.resolve("client", "dist", "index.html") );
-      res.sendFile(path.resolve("client", "dist", "index.html"));
-    });
-  }
 
 export default app;
