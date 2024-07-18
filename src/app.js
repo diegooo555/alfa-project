79bc65a3e406abdd2c7e39812a837932b0593fbf
diegooo555 <diego.sanchez20@uptc.edu.co>
2024-07-18 15:13:11 -0500

    cors settings

--- src/app.js
+++ src/app.js
@@ -10,7 +10,9 @@ const app = express();//server
  
 app.use(cors({
     origin: FRONTEND_URL,
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],  // Métodos HTTP permitidos
+    allowedHeaders: ['Content-Type', 'Authorization'], 
 }));
 
 app.use(morgan('dev'));
